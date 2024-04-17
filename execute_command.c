55191c1238ebb3a627e7807c85da30f3f163bf15
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-04-17 11:32:53 +0000

    redoing dprintf function

--- execute_command.c
+++ execute_command.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * execute_command - a function that execute commands
+ * execute_command - a function that executes commands
  * @commands: commands
  * @filename: filename
  *
@@ -8,21 +8,22 @@
  */
 int execute_command(char **commands, char *filename)
 {
-	pid_t pid;
-	int status = 0;
+    pid_t pid;
+    int status = 0;
 
-	pid = fork();
-	if (pid == 0 && commands[0])
-	{
-		if (execve(commands[0], commands, environ) != -1)
-			return (0);
-		dprintf(STDERR_FILENO, "%s: cannot access '%s': No such file or directory\n",
-				commands[0], filename);
-		exit(2);
-	}
-	else if (pid == -1)
-		perror("Error");
+    pid = fork();
+    if (pid == 0 && commands[0])
+    {
+        if (execve(commands[0], commands, environ) != -1)
+            return (0);
+        fprintf(stderr, "%s: cannot access '%s': No such file or directory\n",
+                commands[0], filename);
+        exit(2);
+    }
+    else if (pid == -1)
+        perror("Error");
 
-	waitpid(pid, &status, 0);
-	return (1);
+    waitpid(pid, &status, 0);
+    return (1);
 }
+
--- requirement_command.c
+++ requirement_command.c
@@ -8,36 +8,37 @@
  */
 int requirement_command(char **commands, char *filename)
 {
-	char *commandWithPath = NULL;
-	int codestatus = 0;
+    char *commandWithPath = NULL;
+    int codestatus = 0;
 
-	if (strcmp(commands[0], "env") == 0)
-	{
-		env_command();
-		return (1);
-	}
+    if (strcmp(commands[0], "env") == 0)
+    {
+        env_command();
+        return (1);
+    }
 
-	if (!is_path(commands[0]))
-	{
-		commandWithPath = path_command(commands[0]);
-		if (!commandWithPath)
-		{
-			dprintf(STDERR_FILENO, "%s: 1: %s: not found\n", filename, commands[0]);
-			return (0);
-		}
-		commands[0] = commandWithPath;
-	}
+    if (!is_path(commands[0]))
+    {
+        commandWithPath = path_command(commands[0]);
+        if (!commandWithPath)
+        {
+            fprintf(stderr, "%s: 1: %s: not found\n", filename, commands[0]);
+            return (0);
+        }
+        commands[0] = commandWithPath;
+    }
 
-	if (access(commands[0], F_OK) == -1)
-	{
-		dprintf(STDERR_FILENO, "%s: 1: %s: not found\n", filename, commands[0]);
-		return (0);
-	}
+    if (access(commands[0], F_OK) == -1)
+    {
+        fprintf(stderr, "%s: 1: %s: not found\n", filename, commands[0]);
+        return (0);
+    }
 
-	codestatus = execute_command(&*commands, filename);
+    codestatus = execute_command(&*commands, filename);
 
-	if (commandWithPath)
-		free(commandWithPath);
+    if (commandWithPath)
+        free(commandWithPath);
 
-	return (codestatus);
+    return (codestatus);
 }
+
