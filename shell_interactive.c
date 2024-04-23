4f4ac631d1ac9b0d0b9925df94706853ed68b7f1
root <hugh.fagan123@gmail.com>
2024-04-23 06:51:00 +0000

    remaining files

--- shell_interactive.c
+++ shell_interactive.c
@@ -2,28 +2,27 @@
 
 /**
  * shell_interactive - prompts for user input
- *  reads, processes commands and repeats until exit/error
+ * reads, processes commands and repeats until exit/error
  * Return: void
- * 
-*/
+ */
 void shell_interactive(void)
 {
-    char *line;
-    char **args;
-    int status = -1;
-    
-    do {
-        printf("simple_prompts$"); /* print prompt symbol */
-        line = read_line(); /* read line from stdin*/
-        args = split_line(line); /* tokenize line*/
-        status = execute_args(args); 
-        /* avoid memory leaks */
-        free(line);
-        free(args);
-        /* exit status */
-        if (status >= 0)
-        {
-            exit (status);
-        }
-    } while (status == -1);
+	char *line;
+	char **args;
+	int status = -1;
+
+	do {
+		printf("simple_prompts$"); /* print prompt symbol */
+		line = read_line(); /* read line from stdin*/
+		args = split_line(line); /* tokenize line*/
+		status = execute_args(args);
+		/* avoid memory leaks */
+		free(line);
+		free(args);
+		/* exit status */
+		if (status >= 0)
+		{
+			exit(status);
+		}
+	} while (status == -1);
 }
